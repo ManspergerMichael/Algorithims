721179749e4bb337e6978361bb14d518da0ccad8
ManspergerMichael <ManspergerMichael@gmail.com>
2018-08-21 13:55:38 -0700

    Added to big13

--- Big13.js
+++ Big13.js
@@ -1,3 +1,8 @@
+/**
+ * The big 13 algorithims taught at Coding Dojo 
+ */
+
+
 //print all numbers from 1 to 255
 function printTo255(){
     for(var i = 1; i<256;i++){
@@ -78,4 +83,67 @@ function squareArray(arr){
     return arr;
 }
 //console.log(squareArray([1,2,3,4,5]));
+
+//given an array of ints and a value Y return count of all ints that are greater than Y
+function returnValGreaterThanY(arr, Y){
+    var count = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > Y){
+            count ++;
+        }
+    }
+    return count;
+}
+//console.log(returnValGreaterThanY([1,2,3,4,5,6,7,8],4));
+
+//change all negitive numbers in an array to 0
+function zeroOutNeg(arr){
+    for(var i = 0;i<arr.length;i++){
+        if(arr[i] < 0){
+            arr[i] = 0;
+        }
+    }
+    return arr;
+}
+//console.log(zeroOutNeg([-1,2,3-4,-5,6,7,8,-9]));
+
+//find the min, max, and average of an array
+function MinMaxAvg(arr){
+    var min = arr[0];
+    var max = arr[0];
+    var sum = 0;
+    for(var i = 0; i<arr.length; i++){
+        sum += arr[i];
+        if(min > arr[i]){
+            min = arr[i];
+        }
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+    avg = sum / arr.length;
+    console.log(min);
+    console.log(max);
+    console.log(avg);
+}
+//MinMaxAvg([1,2,3,4,5,6,7,8,9]);
+
+//shift all array values to the left by one index. Drop the first val and leave 0 at the end
+function shiftVals(arr){
+    for(var i = 0; i<arr.length; i++){
+        if(i == arr.length){
+            arr[i] = 0;
+        }
+        else{
+            arr[i] = arr[i+1];
+        }
+    }
+    return arr;
+}
+//console.log(shiftVals([1,2,3,4,5,6,7,8,9]));
+
+//given an array replace each negitive value with the string "Dojo"
+function swapNegs(arr){
+    
+}
 
