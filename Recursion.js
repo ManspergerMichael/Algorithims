0e72296bf3d3a6b18b57ec6f653648501a4f1f84
ManspergerMichael <ManspergerMichael@gmail.com>
2018-08-22 22:11:23 -0700

    studied a bit of fibonachi recursion

--- Recursion.js
+++ Recursion.js
@@ -1,20 +1,24 @@
 //Recursive sigma
+//take a given number and add each interger value from n down to 1 to result
 function sigma(number){
-    //base case
+    //fast fail
     if(number == 0){
         return 0;
     }
+    //if number is not even, round down
     if(number % 2 != 0){
         number = Math.floor(number);
     }
+    //base case
     if(number == 1){
         return number;
     }
+    //add result of recursive call to number
     else{
         return (number + sigma(number - 1));
     }
 }
-console.log(sigma(5));
+//console.log(sigma(5));
 
 //Recurseive factorial
 function factorial(number){
@@ -26,6 +30,7 @@ function factorial(number){
     if(number % 2 != 0){
         number = Math.floor(number);
     }
+    //base case
     if(number == 1){
         return number;
     }
@@ -33,18 +38,30 @@ function factorial(number){
         return (number * factorial(number - 1));
     }
 }
-console.log(factorial(6));
+//console.log(factorial(6));
 
-
-function fibonacci(num){
+//non-recursive fibonacci sequence 
+function fibonacciLoop(num){
+    //a fibonachi sequence starts with 0 and 1
     var a = 1, b = 0, temp;
-  
+    //loops through calculation until num reaches 0
     while (num >= 0){
+      //store a in temp  
       temp = a;
+      //add the two previous numbers to make the next number in the sequence  
       a = a + b;
+      //store the previous number in b
       b = temp;
+      //reduce number by one
       num--;
     }
-  
     return b;
   }
+  console.log(fibonacciLoop(9));
+
+  //recursive fibonachi sequence
+ function fibonacciRecursion(num){
+    if (num <= 1) return 1;
+    return fibonacciRecursion(num - 1) + fibonacciRecursion(num - 2);
+ }
+ console.log(fibonacciRecursion(9));
--- Strings.js
+++ Strings.js
@@ -37,13 +37,18 @@ function Acronym(str){
 
 //count non-space characters in string
 function countNonSpaces(str){
+    //fast fail if an empty string
     if(str.length <=0){
         return "Error";
     }
+    //split the string into an array, ignoreing whitespace
     var arr = str.split(" ");
+    //initalize character counter
     var result = 0;
     //console.log(arr);
+    //loop through array
     for(var i = 0; i < arr.length;i++){
+        //add length of each string in array to result
         result += arr[i].length;
     }
     return result;
@@ -52,13 +57,19 @@ function countNonSpaces(str){
 //console.log(countNonSpaces("I once took a yoga class that turned out to be a cult."));
 
 function intToRome(number){
+    //dictionary to hold the roman numeral and its interger value
     var numerals ={
         M:1000,D:500,C:100,L:50,X:10,V:5,I:1
     }
-    result = ''; 
+    //result is the string of numerals that will be created in the loop
+    result = '';
+    //for each numeral in dictionary
     for(var i in numerals){
+        //while the given number is greater than the numeral value...
         while (number >=numerals[i]){
+            //add the numeral to the result string
             result += i;
+            //subtract the numeral value from the given number
             number -=numerals[i];
         }
     }
@@ -66,64 +77,3 @@ function intToRome(number){
 }
 //console.log(intToRome(2073));
 
-function isPalindromeOrPermutation(str){
-    //fast fail if string is too short
-    if(str.length < 3){
-        return false;
-    }
-    // is palendrome flag
-    var isPalen = true;
-    //is permutation of palendrome flag
-    var isPermu = true;
-    //dictionary to be used to record what characters are in the string and how many times.
-    var dict = {};
-    //split string into an array for iteration
-    var strLowered = str.toLowerCase();
-    var arr = strLowered.split("");
-    //for loop iterates through length of array, checking for palendrome and creating dictionary at each iteration
-    for(var i = 0; i < arr.length; i++){
-        //runners at both sides of array to check for duplicate characters and if their positions match
-        if(arr[i] != arr[arr.length - i]){
-            // if characters dont match palendrome test fails. No break is used so that the dictionary is created.
-            isPalen = false;
-        }
-        //if character exists in dictionary increment count by one 
-        if(arr[i] in dict){
-            dict[arr[i]] +=1;
-        }
-        //else add character to dictionary, value of one
-        else{
-            dict[arr[i]] = 1;
-        }
-        
-    }
-    //console.log(dict);
-    //test for permutation 
-    //all characters will have a even value except for center character that will appear once
-    var oneCount = 0;
-    for (var key in dict){
-        //if character count is even or %2 = 1 than it is a palendrome permutation
-        if(dict[key] % 2 > 1){
-            isPermu = false;
-            //no further testing needed
-            break;
-        }
-        if(dict[key] == 1){
-            oneCount +=1;
-        }
-    }
-    //if there is more than one character that appears only once in the string than it is not a permutation
-    if(oneCount > 1){
-        isPermu = false;
-    }
-    //if string is a palendrome or a permutation, return true
-    if(isPalen || isPermu){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-//console.log(isPalindromeOrPermutation("racecar"));
-//console.log(isPalindromeOrPermutation("Baboon"));
