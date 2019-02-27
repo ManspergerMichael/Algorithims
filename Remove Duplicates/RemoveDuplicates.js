260de60f130165b0317af91ec1f9ce2712c65a04
Michael Mansperger <ManspergerMichael@gmail.com>
2019-02-26 17:19:57 -0800

    proper answer

--- Remove Duplicates/RemoveDuplicates.js
+++ Remove Duplicates/RemoveDuplicates.js
@@ -1,21 +1,29 @@
 //remove duplicates from sorted array in place
 
 
-removeDuplicates = function(nums) {
-    for(var i = 1; i < nums.length; i++)
+var removeDuplicates = function(nums) {
+    for(var i = 0; i < nums.length; i++)
         {
-            if(nums[i] == nums[i-1])
+            if(nums[i] == nums[i+1])
             {
-                
-                for(var j = i; j < nums.length; j++)
+                /* The processed section of the array is held in place at i.
+                   while nums[i] == nums[i+1] the nested loop will continue to shift 
+                   the array.
+                */
+                while(nums[i] == nums[i+1])
                     {
-                        nums[j] = nums[j-1];
+                        for(var j = i+1; j < nums.length; j++)
+                        {
+                            nums[j] = nums[j+1];
+                        }
+                        nums.pop();
                     }
-                nums.pop();
+                
             }
         }
     console.log(nums.length);
     return nums.length;
 };
 var nums = [1,1,2];
+var nums2 = [0,0,1,1,1,2,2,3,3,4]
 console.log(removeDuplicates(nums));
