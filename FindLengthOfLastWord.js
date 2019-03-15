934590708d7c4f05b5ee24366c5a9921a41ff73c
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-14 23:59:26 -0700

    Find length of last word in string

---
+++ FindLengthOfLastWord.js
@@ -0,0 +1,16 @@
+/**
+ * find the length of the last word in the given string
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    var arr = s.split(" ");
+    for(var i = arr.length - 1; i > -1; i--)
+        {
+            if(arr[i].length > 0){
+                return arr[i].length;
+            }
+        }
+    return 0;
+    
+};
