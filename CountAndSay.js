abb85a740f04b4657e8152afd3bc74b50fc8b89b
Michael Mansperger <ManspergerMichael@gmail.com>
2019-04-10 12:39:33 -0700

    count&Say
    first attempt

---
+++ CountAndSay.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+
+ /**
+  * My first attempt at the problem,
+  * I mis-read the instructions.
+  * I'm keeping this as an example of javaScriupt dictionaries.
+  */
+
+var countAndSayFirst = function(n) {
+    if(n === 1)
+        {
+            return "1";
+        }
+    var dict = {};
+    var arr = (""+n).split("");
+    var result = 0;
+    for(var i = 0; i < arr.length; i++)
+        {
+            if( arr[i] in dict)
+            {
+                dict[arr[i]] += 1;
+            }
+            else
+            {
+                dict[arr[i]] = 1;
+            }
+        }
+    for(var key in dict)
+        {
+            console.log("The key is "+key+" The value is " + dict[key]);
+            result *= 10;
+            result += dict[key];
+            result += key;
+        }
+    return result.toString();
+};
